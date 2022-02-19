12985b3a94a66ffab52cd0a185d526e457e83737
Swati0599 <65229725+Swati0599@users.noreply.github.com>
2022-02-19 20:12:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const body = Matter.Body;
+const Constraint = Matter.Constraint;
+var engine, world;
+
+
+function preload() {
+  
+}
+
+function setup() {
+  canvas = createCanvas(1200,600);
+  engine = Engine.create();
+  world = engine.world;
+  
+
+}
+
+function draw() {
+  background(189);
+  
+  Engine.update(engine);
+ 
+ 
+
+ 
+}
+
+
+
+
+
+    
+  
